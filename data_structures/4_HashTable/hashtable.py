7f089b2826e62a5ed4a185a6acb2bc77078d15ae
sanny hong <synhong43@gmail.com>
2024-01-08 16:51:58 +0800

    add hashTable.py

---
+++ data_structures/4_HashTable/hashtable.py
@@ -0,0 +1,22 @@
+class HashTable:
+    def __init__(self):
+        self.capacity = 8  # initial capacity
+        self.size = 0  # number of key-value pairs
+        self.buckets = [[] for _ in range(self.capacity)]
+
+    def hashing(self, key):
+        "Basic "
+        return
+
+    def resize(self):
+        return
+
+    def insert(self, key, value):
+        return
+
+    def get(self, key):
+        return
+
+    def delete(self, key):
+        return
+
